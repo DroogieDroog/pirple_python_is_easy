e273cfff814078cca01b1155d2bcf3ea29a35dcb
Jens Lisinski <jens.lisinski@gmail.com>
2021-03-08 08:57:29 -0500

    final-project: Initial conversion to Go Fish

--- final_project/main.py
+++ final_project/main.py
@@ -2,7 +2,7 @@
 pirple/python/final_project/main.py
 Final Project
 
-Create a gin rummy game
+Create a Go Fish game
 """
 
 from random import (shuffle, randint)
@@ -16,16 +16,11 @@ class Player:
         self.Name = name
         self.Computer = computer
         self.Hand = {}
-        self.Game = 0
-        self.Round = 0
-        self.Deadwood = 0
-        self.Groups = {}
+        self.Sets = {}
 
-    def new_hand(self, hand, round=0, deadwood=0, groups={}):
+    def new_hand(self, hand, sets={}):
         self.Hand = hand
-        self.Round = round
-        self.Deadwood = deadwood
-        self.Groups = groups
+        self.Sets = sets
 
     def print_hand(self):
         if self.Name.upper().endswith('S'):
@@ -36,29 +31,41 @@ class Player:
         for denom in sorted(self.Hand):
             for suit in self.Hand[denom]:
                 if self.Computer:
-                    print('\u2733', end=' ')
+                    print(card_map[denom] + suit, end=' ')
+#                    print('\u2733', end=' ')
                 else:
                     print(card_map[denom] + suit, end=' ')
         print()
+        self.print_sets()
 
-    def draw(self, source):
-        draw_card = source.pop()
+    def print_sets(self):
+        pass
+
+    def draw(self, card_deck, card_map, wish):
+        draw_card = card_deck.pop()
+
         if draw_card[0] in self.Hand.keys():
             self.Hand[draw_card[0]].append(draw_card[1])
         else:
             self.Hand.update({draw_card[0]: [draw_card[1]]})
-        self.print_hand()
 
-    def discard(self, hand):
-        pass
-        #return self.Hand
+        if draw_card[0] == card_map[wish]:
+            print('Fish, fish, you got your wish!')
+            self.print_hand()
+            return True
+        else:
+            print('Booooo, you didn\'t get your wish.')
+            return False
 
-    def knock(self):
-        pass
+    def lay_set(self):
+        for card, suits in self.Hand.items():
+            if len(suits) == 4:
+                self.Sets.update(self.Hand.pop(card))
 
-    def score(self, hand):
-        pass
-        #return self.Score, self.Round, self.Deadwood
+        if len(self.Hand) == 0:
+            return True
+        else:
+            return False
 
 
 def clear_screen():
@@ -109,7 +116,7 @@ def deal_hand(deck, players, new_game=True, winner=None):
     hands.append({})
     hands.append({})
 
-    for card in range(10):
+    for card in range(7):
         for hand in range(2):
             top_card = deck.pop()
             if top_card[0] in hands[hand].keys():
@@ -130,13 +137,6 @@ def deal_hand(deck, players, new_game=True, winner=None):
 
 def start_new_game():
     while(True):
-        game_target = input('Would you like to play each round to 100 or 200 points? ')
-        if int(game_target) not in (100, 200):
-            print('You must enter 100 or 200.  Enter again.')
-        else:
-            break
-
-    while(True):
         player_name = input('Please enter your name: ')
         yn = input('You\'ve chosen {} for your name. Is that correct (y/n)? '.format(player_name))
         if yn.upper() == 'Y':
@@ -148,40 +148,34 @@ def start_new_game():
     players = [Player(player_name), Player('Computer', True)]
     card_deck, card_map = create_deck()
     dealer, players[0].Hand, players[1].Hand = deal_hand(card_deck, players)
-    discard_pile = [card_deck.pop()]
 
-    return players, dealer, card_deck, card_map, discard_pile, int(game_target)
+    return players, dealer, card_deck, card_map
 
 
-def display_current_status(players, card_deck, discard_pile):
+def display_current_status(players, card_deck):
     break_line = '\u274C' * 25
     cards_left = len(card_deck)
-    if len(discard_pile) > 0:
-        top_discard = card_map[discard_pile[-1][0]] + discard_pile[-1][1]
-    else:
-        top_discard = '<Empty>'
 
     clear_screen()
     print(break_line + '\n')
     players[1].print_hand()
     print()
-    print('Draw pile: {} cards   Discard: {}'.format(cards_left, top_discard))
+    print('Draw pile: {} cards'.format(cards_left))
     print()
     players[0].print_hand()
     print('\n' + break_line)
 
 
-def play_game(players, dealer, card_deck, discard_pile, game_target):
-    round_complete = False
+def play_game(players, dealer, card_deck):
+    game_over = False
 
     if dealer == 0:
         current_player = 1
     else:
         current_player = 0
 
-    while not round_complete:
-        display_current_status(players, card_deck, discard_pile)
-        round_complete = play_hand(players[current_player], card_deck, discard_pile)
+    while not game_over:
+        game_over = play_hand(players, current_player, card_deck, card_map)
 
         if current_player == 0:
             current_player = 1
@@ -189,38 +183,36 @@ def play_game(players, dealer, card_deck, discard_pile, game_target):
             current_player = 0
 
 
-def play_hand(player, card_deck, discard_pile):
-    if len(discard_pile) > 0:
-        print('{}, would you like to draw from the deck (D) or discard pile (P)?'.format(player.Name))
-        while(True):
-            draw_source = input('Make your choice: ')
-            if draw_source.upper() not in ('D', 'P'):
-                print('You must choose either the (D)eck or discard (P)ile to draw from.')
+def play_hand(players, current_player, card_deck, card_map):
+    got_wish = True
+    game_over = False
+    display_current_status(players, card_deck)
+    player = players[current_player]
+    print('Your turn, {}.'.format(player.Name))
+
+    while got_wish and not game_over:
+        while True:
+            wish = input('What is your wish? ')
+            if wish.upper() not in card_map.values():
+                print('You must wish for a valid card value (2-10, J, Q, K, A)')
             else:
                 break
-    else:
-        print('Discard pile is empty. Automatically drawing from the deck for {}.'.format(player.Name))
-        draw_source = 'D'
 
-    if draw_source.upper() == 'D':
-        player.draw(card_deck)
-    else:
-        player.draw(discard_pile)
+        got_wish = player.draw(card_deck, card_map, wish)
+        game_over = player.lay_set()
 
-    return False
+    return game_over
 
 
 def main():
-    global card_deck, card_map, discard_pile
+    global card_deck, card_map
     clear_screen()
-    print('Welcome to JFL Gin Rummy! This is a one-player game against the Computer.')
+    print('Welcome to JFL Go Fish! This is a one-player game against the Computer.')
 
-    players, dealer, card_deck, card_map, discard_pile, game_target = start_new_game()
-    play_game(players, dealer, card_deck, discard_pile, game_target)
+    players, dealer, card_deck, card_map = start_new_game()
+    play_game(players, dealer, card_deck)
     x = 1
 
 
 if __name__ == '__main__':
     main()
-
-
